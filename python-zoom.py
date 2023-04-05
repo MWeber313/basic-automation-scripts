a469ef98294d29c87454c3dd729f6597893a2de1
MWeberLambdaweb19 <mack.webb37@gmail.com>
2023-04-04 20:38:03 -0700

    Meetings successfully launch

--- python-zoom.py
+++ python-zoom.py
@@ -28,31 +28,31 @@ API_SECRET = os.getenv('API_SECRET')
 # API_KEY = line
 # API_SEC = line2
 
-print(API_KEY)
-print(API_SECRET)
-
 ## MW END
 
 
 # create a function to generate a token
 # using the pyjwt library
 
+# MW START (I rewrote the generateToken function using a different JWT library)
 
 def generateToken():
-	token = jwt.encode(
+	encoded = jwt.encode({'iss': API_KEY, 'exp': time() + 5000}, API_SECRET, algorithm="HS256")
+	return encoded
 
-		# Create a payload of the token containing
-		# API Key & expiration time
-		{'iss': API_KEY, 'exp': time() + 5000},
+	# jwtInstance = jwt.JWT()
 
-		# Secret used to generate token signature
-		API_SECRET,
+	# # Create a payload of the token containing
+	# # API Key & expiration time
+	# jwtMessage = {'iss': API_KEY, 'exp': time() + 5000},
+	# # create a token using the api secret, the api key, and a hashing algo
 
-		# Specify the hashing alg
-		algorithm='HS256'
-	)
-	return token.decode('utf-8')
+	# # Secret used to generate token signature
+	# # Specify the hashing alg
+	# token = jwtInstance.encode(jwtMessage, API_SECRET, alg='HS256')
+	# return token.decode('utf-8')
 
+# MW END
 
 # create json data for post requests
 meetingdetails = {
@@ -75,30 +75,29 @@ meetingdetails = {
         "mute_upon_entry": "false",
         "watermark": "true",
         "audio": "voip",
-        "auto_recording": "cloud"
+        "auto_recording": "none"
     }
 }
 
 # send a request with headers including
 # a token and meeting details
 
+generateToken()
+
 def createMeeting():
 	headers = {'authorization': 'Bearer ' + generateToken(),
 			'content-type': 'application/json'}
-	r = requests.post(
+
+	response = requests.post(
 		f'https://api.zoom.us/v2/users/me/meetings',
 		headers=headers, data=json.dumps(meetingdetails))
 
-	print("\n creating zoom meeting ... \n")
-	# print(r.text)
-	# converting the output into json and extracting the details
-	y = json.loads(r.text)
-	join_URL = y["join_url"]
-	meetingPassword = y["password"]
+	solution = json.loads(response.text)
+	join_URL = solution["join_url"]
 
-	print(
-		f'\n here is your zoom meeting link {join_URL} and your \
-		password: "{meetingPassword}"\n')
+	URL = print(f'{join_URL}')
+
+	return URL
 
 
 # run the create meeting function
