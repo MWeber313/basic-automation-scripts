c5a8fcea2ac1a5e74678849522ba97e4c479ff8c
MWeberLambdaweb19 <mack.webb37@gmail.com>
2023-04-22 17:51:10 -0700

    Can edit a file individually by file name, looking into ways I can edit the entire directory at once

--- mech-weapon-modder.py
+++ mech-weapon-modder.py
@@ -1,6 +1,30 @@
 import json
+import os
 
-with open('D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon\Weapon_Autocannon_AC2_0-STOCK - Copy.json') as file_object:
-    for line in file_object:
-        print(line)
+# with open('D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon\Weapon_Autocannon_AC2_0-STOCK-Copy.json', 'r+') as file_object:
+#     contents = json.load(file_object)
+#     contents['Tonnage'] = 10
+#     file_object.seek(0)
+#     json.dump(contents, file_object, indent=4)
+#     file_object.truncate()
+
+# print(contents['Tonnage'])
+
+os.chdir('D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon')
+
+current_dir = os.getcwd()
+
+for file in current_dir:
+    print(file)
+
+# for file in current_dir:
+#     with open(file, 'r+') as file:
+#         contents = json.load(file)
+#         print(contents['Tonnage'])
+#     # contents['Tonnage'] = 1
+#     # print('Current Tonnage: ', contents['Tonnage'])
+
+
+
+# print(os.getcwd())
 
