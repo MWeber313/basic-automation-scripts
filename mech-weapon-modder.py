36b0f0167bd811be58d9b250ae91a12984dcd219
MWeberLambdaweb19 <mack.webb37@gmail.com>
2023-04-22 17:33:12 -0700

    Working on a file modifier for the game Battletech Heavy Metal

---
+++ mech-weapon-modder.py
@@ -0,0 +1,6 @@
+import json
+
+with open('D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon\Weapon_Autocannon_AC2_0-STOCK - Copy.json') as file_object:
+    for line in file_object:
+        print(line)
+
