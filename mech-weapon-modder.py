6b1ad080077f7a7472c850b4a5e2555dca6d8b50
MWeberLambdaweb19 <mack.webb37@gmail.com>
2023-04-25 23:12:27 -0700

    program works!

--- mech-weapon-modder.py
+++ mech-weapon-modder.py
@@ -12,10 +12,15 @@ import os
 
 os.chdir('D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon')
 
-current_dir = os.getcwd()
+current_dir = os.listdir('D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon')
 
 for file in current_dir:
-    print(file)
+    with open(f'D:\Games\BATTLETECH Heavy Metal\BattleTech_Data\StreamingAssets\data\weapon\{file}', 'r+') as file_object:
+        contents = json.load(file_object)
+        contents['Tonnage'] = 1
+        file_object.seek(0)
+        json.dump(contents, file_object, indent=4)
+        file_object.truncate()
 
 # for file in current_dir:
 #     with open(file, 'r+') as file:
